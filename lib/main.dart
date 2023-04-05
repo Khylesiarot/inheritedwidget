e01093c9e1fdd63dee640a5936d256f7bb26a799
khyle_siarot <johnkhyle.siarot.20@usjr.edu.ph>
2023-04-05 23:24:05 +0800

    updated title

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,48 @@
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
-    ));
+     debugShowCheckedModeBanner: false,
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: const HomePage(),
+  ));
 }
-class HomePage extends StatelessWidget {
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  String tittle = "Tap the screen";
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
-      appBar: AppBar(title: const Text("sIAROT CUttie")),
-);
+    return Scaffold(
+      appBar: AppBar(
+        title:  Text(tittle),
+        centerTitle: true,
+      ),
+      body: GestureDetector(
+          onTap: (){
+            setState(() {
+              tittle = DateTime.now().toIso8601String();
+            });
+          },
+
+
+
+
+        child: Container(
+          color: Colors.white,
+        ),
+      ),
+    );
   }
 }
